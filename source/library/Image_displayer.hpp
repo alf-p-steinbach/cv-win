e89dbcfb2bdb2973e49a76a938c67ab98cb71570
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2020-05-26 01:35:50 +0200

    -

---
+++ source/library/Image_displayer.hpp
@@ -0,0 +1,35 @@
+﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+#include <winapi-integration/Abstract_image_displayer.hpp>
+#include <cv-win/display.hpp>
+
+namespace cv_win {
+    $use_cppx( Size );
+    using winapi::integration::Abstract_image_displayer;
+
+    class Image_displayer:
+        public Abstract_image_displayer
+    {
+        cv::Mat     m_image;
+
+    public:
+        auto width() const
+            -> Size override
+        { return m_image.cols; }
+
+        auto height() const
+            -> Size override
+        { return m_image.rows; }
+
+        void display_on( const HDC dc )
+        {
+            display_bgr( dc, m_image );
+        }
+
+        void stretch_display_on( const HDC dc, const RECT& rect )
+        {
+            $is_unused( dc );  $is_unused( rect );
+            $fail( "Not implemented yet" );
+        }
+    };
+
+}  // namespace cv_win
